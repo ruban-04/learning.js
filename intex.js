9f3b4d66f4e74eb9debe4d5eadf477c2f724cf8f
ruban <kavitharuban3002@gmail.com>
2024-09-03 16:55:16 +0530

    operatoers

--- intex.js
+++ intex.js
@@ -1,2 +1,35 @@
-var a = 20
-console . log(a)
+function hello()
+{
+document.getElementById('dem').innerHTML='date';
+}
+var price = 34500
+var product = "mobile"
+var tax = 500
+console.log(product)
+var total=price+tax;
+console.log(total)
+
+var fruitname = "orange"
+var count = 6
+var price = 70
+console.log(fruitname)
+console.log(70*6)
+
+// function
+
+function Blue(){
+    console.log("premium")
+}
+Blue()
+function red(){
+    console.log("coding")
+}
+function yellow(){
+    console.log("courses")
+}
+function green(){
+    console.log("programiz")
+}
+red()
+yellow()
+green()
