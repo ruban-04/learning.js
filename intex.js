55c6bd36e2555241b47537b6ed8224b2004fb913
ruban <kavitharuban3002@gmail.com>
2024-09-05 16:54:08 +0530

    using return function

--- intex.js
+++ intex.js
@@ -34,7 +34,89 @@ red()
 yellow()
 green()
 
+var a=10
+var b=15
+
+function add()
+{
+    console.log(a+b)
+}
+add()
+
+
+
 // type
 
 console.log(typeof(true))
 console.log(typeof(false))
+
+// Array
+let vehicle= ["car", "bus", "bike", "van" ,"lorry"];
+
+    console.log(vehicle[0])
+    console.log(vehicle[1])
+    console.log(vehicle[2])
+    console.log(vehicle[3])
+    console.log(vehicle[4])
+
+    var factor="vijay";
+    var fplayer="doni";
+    var fmovie="naam";
+    
+    function favourite()
+    {
+        console.log("my fav actor:", factor);
+         console.log("my fav actor:", fplayer);
+          console.log("my fav actor:", fmovie);
+    }
+       favourite()
+
+       function area(l,b)
+    {
+    
+       var a=l*b
+       console.log("Area is:"+a)
+    }
+       area(25,35)
+
+       function hello()
+       {
+       document.getElementById('dem').innerHTML='date';
+       }
+       var price = 34500
+       var product = "mobile"
+       var tax = 500
+       console.log(product)
+       var total=price+tax;
+       console.log(total)
+       
+       var fruitname = "orange"
+       var count = 6
+       var price = 70
+       console.log(fruitname)
+       console.log(70*6)
+       function Blue(){
+        console.log("premium")
+    }
+    Blue()
+    function red(){
+        console.log("coding")
+    }
+    function yellow(){
+        console.log("courses")
+    }
+    function green(){
+        console.log("programiz")
+    }
+    red()
+    yellow()
+    green()
+    
+    var a=10
+    var b=15
+    
+    function add()
+    {
+        console.log(a+b)
+    }
+    add()
