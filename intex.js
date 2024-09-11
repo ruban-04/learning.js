99b77d07433c3cd0d01ec16488e3f8f881975a8f
ruban <kavitharuban3002@gmail.com>
2024-09-11 16:54:37 +0530

    using for loops

--- intex.js
+++ intex.js
@@ -203,3 +203,44 @@ function checkOddOrEven(number){
         
     }
      checkOddOrEven("e")
+
+
+// eligible or not eligible
+
+let name = "Bavya";
+let age = 18;
+
+if (age >= 18) {
+    console.log(name + " is eligible to vote.");
+}
+
+
+else {
+    console.log(name + " is not eligible to vote.");
+}
+
+// for loop
+for (count=1;count<=10;count=count+1)
+    {
+        console.log("Ruban")
+    }
+    for (count=1;count<=20;count=count+1)
+        {
+            console.log(count)
+        }
+       
+        for (count=10;count>=1;count=count-1)
+            {
+                console.log(count)
+            } 
+            for (count=1;count<=20;count=count+2)
+                {
+                    console.log(count)
+                }
+                for (count=1;count<=10;count=count+1)
+                    {
+                        console.log(count+" x2="+(count*2))
+                        }
+
+
+                     
